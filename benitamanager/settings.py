de186c81ded82e64f3b47e73338a7a3f12820db5
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2017-07-05 02:18:29 +0100

    Added files and changes required for deployment to heroku

--- benitamanager/settings.py
+++ benitamanager/settings.py
@@ -120,13 +120,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),]
 
 # Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
+#https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 LOGIN_REDIRECT_URL = '/benitarecall/add_patient/'
 
